70e8ffc00f8820c44b632ee3be041adba2ba6388
unknown <876700669@qq.com>
2018-08-23 21:25:24 +0800

    finish BaseInfoLayer.cpp and h

--- BaseInfoLayer.h
+++ BaseInfoLayer.h
@@ -1,1 +1,19 @@
 #pragma once
+#ifndef _BASE_INFO_LAYER_H_
+#define _BASE_INFO_LAYER_H_
+
+#include "cocos2d.h"
+#include "BaseDoll.h"
+
+USING_NS_CC;
+
+class BaseInfoLayer:public Sprite
+{
+public:
+	static BaseInfoLayer* createBaseInfoLayer(DollType dolltype);
+	virtual bool init(DollType dollType);
+	void setBaseInfo(DollType dollType);
+	Sprite * info;
+      
+};
+#endif
